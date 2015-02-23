9edf5117f99b4286d4b240af94d9651a9f5c6fde
Jakob <jaklindq@student.chalmers.se>
2015-02-23 11:43:40 +0100

    I Canonical.m är historien nu slumpad. Dessutom finns i analysen en koll av självkorrelansen för logReturn

--- CanonicalAgents.m
+++ CanonicalAgents.m
@@ -1,10 +1,12 @@
-function [ agents ] = CanonicalAgents(N,memory,s)
+function [agents] = CanonicalAgents(N,memory,nbrOfStrategies)
 
 m = 2^memory;
-agents = zeros(N,m,s);
+agents = zeros(N,m,nbrOfStrategies);
+%points = (epsilon+1)*ones(nbrOfAgents, nbrOfStrategies);
+
     for i=1:N
 
-        agents(i,:,:) =ones(m,s) - floor(rand(m,s)*2)*2;
+        agents(i,:,:) =ones(m,nbrOfStrategies) - floor(rand(m,nbrOfStrategies)*2)*2;
         
     end
     
